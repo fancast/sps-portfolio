0dd6fda9005df59c77725a139d81e9bf04aa1986
Jerry Jean-Pierre <jeanpierrejerry@.com>
2020-03-09 01:44:34 -0400

    Add google map and new subheading places to main page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -50,3 +50,9 @@ function createParagraphElement(text) {
   pElement.innerText = text;
   return pElement;
 }
+
+function createMap() {
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
